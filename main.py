c14f6440d1287c92deeb998f56f2ccd9ac723d02
Serpensin <wissmann.matti@gmail.com>
2023-06-14 00:26:13 +0200

    added setup command

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-#Import
+﻿#Import
 import asyncio
 import discord
+import io
 import json
 import jsonschema
 import logging
 import logging.handlers
 import os
 import platform
+import random
 import sentry_sdk
+import sqlite3
+import string
 import sys
 import time
+from captcha.image import ImageCaptcha
 from datetime import timedelta, datetime
 from dotenv import load_dotenv
 from zipfile import ZIP_DEFLATED, ZipFile
@@ -18,14 +23,15 @@ from zipfile import ZIP_DEFLATED, ZipFile
 
 #Init
 load_dotenv()
+image_captcha = ImageCaptcha()
 sentry_sdk.init(
     dsn=os.getenv('SENTRY_DSN'),
     traces_sample_rate=1.0,
     profiles_sample_rate=1.0,
-    environment='Production'
+    environment='Development'
 )
-app_folder_name = 'BOTFOLDER'
-bot_name = 'BOTNAME'
+app_folder_name = 'Bouncer'
+bot_name = 'Bouncer'
 if not os.path.exists(f'{app_folder_name}//Logs'):
     os.makedirs(f'{app_folder_name}//Logs')
 if not os.path.exists(f'{app_folder_name}//Buffer'):
@@ -44,8 +50,8 @@ logging.getLogger('discord.http').setLevel(logging.INFO)
 handler = logging.handlers.RotatingFileHandler(
     filename = f'{log_folder}{bot_name}.log',
     encoding = 'utf-8',
-    maxBytes = 8 * 1024 * 1024, 
-    backupCount = 5,            
+    maxBytes = 8 * 1024 * 1024,
+    backupCount = 5,
     mode='w')
 dt_fmt = '%Y-%m-%d %H:%M:%S'
 formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
@@ -54,9 +60,28 @@ logger.addHandler(handler)
 manlogger.addHandler(handler)
 manlogger.info('Engine powering up...')
 
+#SQLite init
+conn = sqlite3.connect(f'{app_folder_name}//{bot_name}.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS servers (
+            guild_id INTEGER PRIMARY KEY,
+            verify_channel INTEGER,
+            verify_role INTEGER,
+            log_channel INTEGER,
+            timeout INTEGER,
+            action TEXT
+            )
+        ''')
+c.execute('''CREATE TABLE IF NOT EXISTS panels (
+            guild_id INTEGER PRIMARY KEY,
+            panel_id INTEGER
+            )
+        ''')
+
 #Load env
 TOKEN = os.getenv('TOKEN')
 ownerID = os.getenv('OWNER_ID')
+support_id = os.getenv('SUPPORT_SERVER')
 
 #Create activity.json if not exists
 class JSONValidator:
@@ -64,7 +89,7 @@ class JSONValidator:
         "type" : "object",
         "properties" : {
             "activity_type" : {
-                "type" : "string", 
+                "type" : "string",
                 "enum" : ["Playing", "Streaming", "Listening", "Watching", "Competing"]
             },
             "activity_title" : {"type" : "string"},
@@ -108,13 +133,11 @@ validator = JSONValidator(activity_file)
 validator.validate_and_fix_json()
 
 
+#Bot Class
 class aclient(discord.AutoShardedClient):
     def __init__(self):
 
         intents = discord.Intents.default()
-        intents.guild_messages = True
-        intents.dm_messages = True
-        intents.members = True
 
         super().__init__(owner_id = ownerID,
                               intents = intents,
@@ -141,7 +164,7 @@ class aclient(discord.AutoShardedClient):
                 return discord.Activity(type=discord.ActivityType.watching, name=activity_title)
             elif activity_type == 'Competing':
                 return discord.Activity(type=discord.ActivityType.competing, name=activity_title)
-    
+
         @staticmethod
         def get_status() -> discord.Status:
             with open(activity_file) as f:
@@ -183,6 +206,7 @@ tree = discord.app_commands.CommandTree(bot)
 
 # Check if all required variables are set
 owner_available = bool(ownerID)
+support_available = bool(support_id)
 
 #Fix error on windows on shutdown
 if platform.system() == 'Windows':
@@ -193,16 +217,18 @@ def clear():
     else:
         os.system('clear')
 
+
+
 ##Events
 class Events():
     @bot.event
     async def on_guild_remove(guild):
         manlogger.info(f'I got kicked from {guild}. (ID: {guild.id})')
-    
+
     @bot.event
     async def on_guild_join(guild):
         manlogger.info(f'I joined {guild}. (ID: {guild.id})')
-    
+
     @tree.error
     async def on_app_command_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError) -> None:
         options = interaction.data.get("options")
@@ -211,7 +237,7 @@ class Events():
             for option in options:
                 option_values += f"{option['name']}: {option['value']}"
         if isinstance(error, discord.app_commands.CommandOnCooldown):
-            await interaction.response.send_message(f'This command is on cooldown.\nTime left: `{await Functions.seconds_to_minutes(error.retry_after)}`.', ephemeral=True)
+            await interaction.response.send_message(f'This command is on cooldown.\nTime left: `{str(timedelta(seconds=error.retry_after))}`.', ephemeral=True)
         else:
             try:
                 await interaction.followup.send(f"{error}\n\n{option_values}", ephemeral=True)
@@ -219,24 +245,85 @@ class Events():
                 await interaction.response.send_message(f"{error}\n\n{option_values}", ephemeral=True)
             manlogger.warning(f"{error} -> {option_values} | Invoked by {interaction.user.name} ({interaction.user.id})")
 
+    @bot.event
+    async def on_interaction(interaction: discord.Interaction):
+        #Captcha Info View
+        class WhyView(discord.ui.View):
+            def __init__(self, *, timeout=None):
+                super().__init__(timeout=timeout)
+
+                self.add_item(discord.ui.Button(label='Secure your server', url = f'https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=268503046&scope=bot%20applications.commands', style=discord.ButtonStyle.link))
+
+
+        if interaction.data and interaction.data.get('component_type') == 2:  # 2 is the component type for button
+            button_id = interaction.data.get('custom_id')
+
+            if button_id == 'verify':
+                await interaction.response.send_message('Verified', ephemeral=True)
+            elif button_id == 'why':
+                await interaction.response.send_message(f'This serever is protected by <@!{bot.user.id}> to prevent raids & malicious users.\n\nTo gain access to this server, you\'ll need to verify yourself by completing a captcha.\n\nYou don\'t need to connect your account for that.', view = WhyView(), ephemeral=True)
+
+
 
 #Functions
 class Functions():
-    def seconds_to_minutes(input_int):
-        return(str(timedelta(seconds=input_int)))
+    def create_captcha():
+        captcha_text = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+        data = image_captcha.generate(captcha_text)
+        image_captcha.write(captcha_text, f'{app_folder_name}{captcha_text}.png')
+        return io.BytesIO(data.read()), captcha_text
 
- 
+
+    async def create_support_invite(interaction):
+        try:
+            guild = bot.get_guild(int(support_id))
+        except ValueError:
+            return "Could not find support guild."
+        if guild is None:
+            return "Could not find support guild."
+        if not guild.text_channels:
+            return "Support guild has no text channels."
+        try:
+            member = await guild.fetch_member(interaction.user.id)
+        except discord.NotFound:
+            member = None
+        if member is not None:
+            return "You are already in the support guild."
+        channels: discord.TextChannel = guild.text_channels
+        for channel in channels:
+            try:
+                invite: discord.Invite = await channel.create_invite(
+                    reason=f"Created invite for {interaction.user.name} from server {interaction.guild.name} ({interaction.guild_id})",
+                    max_age=60,
+                    max_uses=1,
+                    unique=True
+                )
+                return invite.url
+            except discord.Forbidden:
+                continue
+            except discord.HTTPException:
+                continue
+        return "Could not create invite. There is either no text-channel, or I don't have the rights to create an invite."
+
+
+
+
+
+
+
 ##Owner Commands
 #Shutdown
-@tree.command(name = 'shutdown', description = 'Savely shut down the bot.')
-async def self(interaction: discord.Interaction):
-    if interaction.user.id == int(ownerID):
-        manlogger.info('Engine powering down...')
-        await bot.change_presence(status = discord.Status.invisible)
-        await interaction.response.send_message('Engine powering down...', ephemeral = True)
-        await bot.close()
-    else:
-        await interaction.response.send_message('Only the BotOwner can use this command!', ephemeral = True)
+if owner_available:
+    @tree.command(name = 'shutdown', description = 'Savely shut down the bot.')
+    async def self(interaction: discord.Interaction):
+        if interaction.user.id == int(ownerID):
+            manlogger.info('Engine powering down...')
+            await bot.change_presence(status = discord.Status.invisible)
+            conn.close()
+            await interaction.response.send_message('Engine powering down...', ephemeral = True)
+            await bot.close()
+        else:
+            await interaction.response.send_message('Only the BotOwner can use this command!', ephemeral = True)
 
 
 #Get Logs
@@ -417,9 +504,9 @@ async def self(interaction: discord.Interaction):
     embed.add_field(name="discord.py-Version", value=f"{discord.__version__}", inline=True)
     embed.add_field(name="Sentry-Version", value=f"{sentry_sdk.consts.VERSION}", inline=True)
 
-    embed.add_field(name="Repo", value=f"[GitLab](https://gitlab.bloodygang.com/Serpensin/Discord-Bot-Bouncer)", inline=True)
-    embed.add_field(name="Invite", value=f"[Invite me](https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=8&scope=bot%20applications.commands)", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)  
+    embed.add_field(name="Repo", value=f"[GitLab](https://gitlab.bloodygang.com/Serpensin/DiscordBots-Bouncer)", inline=True)
+    embed.add_field(name="Invite", value=f"[Invite me](https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=268503046&scope=bot%20applications.commands)", inline=True)
+    embed.add_field(name="\u200b", value="\u200b", inline=True)
 
     await interaction.response.send_message(embed=embed)
 
@@ -432,6 +519,150 @@ async def self(interaction: discord.Interaction):
 async def self(interaction: discord.Interaction, nick: str):
     await interaction.guild.me.edit(nick=nick)
     await interaction.response.send_message(f'My new nickname is now **{nick}**.', ephemeral=True)
+
+
+#Support Invite
+if support_available:
+    @tree.command(name = 'support', description = 'Get invite to our support server.')
+    @discord.app_commands.checks.cooldown(1, 60, key=lambda i: (i.user.id))
+    async def self(interaction: discord.Interaction):
+        if str(interaction.guild.id) != support_id:
+            await interaction.response.defer(ephemeral = True)
+            await interaction.followup.send(await Functions.create_support_invite(interaction), ephemeral = True)
+        else:
+            await interaction.response.send_message('You are already in our support server!', ephemeral = True)
+
+
+#Send pannel
+@tree.command(name = 'send_pannel', description = 'Send pannel to varification channel.')
+@discord.app_commands.checks.cooldown(1, 60, key=lambda i: (i.guild_id))
+@discord.app_commands.checks.has_permissions(manage_guild = True)
+async def self(interaction: discord.Interaction):
+    await interaction.response.defer(ephemeral= True)
+    #Captcha Info View
+    class CaptchaView(discord.ui.View):
+        def __init__(self, *, timeout=None):
+            super().__init__(timeout=timeout)
+
+            self.add_item(discord.ui.Button(label='🤖 Verify', style=discord.ButtonStyle.blurple, custom_id='verify'))
+            self.add_item(discord.ui.Button(label='Why?', style=discord.ButtonStyle.blurple, custom_id='why'))
+
+
+    embed = discord.Embed(title = ':robot: Verification required',
+                          description = f'To proceed to `{interaction.guild.name}`, we kindly ask you to confirm your humanity by solving a captcha. Simply click the button below to get started!',
+                          color = 0x2b63b0)
+
+    c.execute('SELECT * FROM servers WHERE guild_id = ?', (interaction.guild.id,))
+    data = c.fetchone()
+    if data:
+        verify_channel_id = data[1]
+    else:
+        verify_channel_id = None
+    try:
+        verify_channel = await bot.fetch_channel(verify_channel_id)
+    except discord.NotFound:
+        verify_channel = None
+    except discord.Forbidden:
+        await interaction.followup.send(f'I don\'t have permission to see the verification channel (<#{verify_channel_id}>).', ephemeral = True)
+        return
+    if not verify_channel:
+        await interaction.followup.send('The verification channel is not set or doesn\'t exist. Please set it with `/setup`.', ephemeral = True)
+        return
+
+    c.execute('SELECT * FROM panels WHERE guild_id = ?', (interaction.guild_id,))
+    data = c.fetchone()
+    if data:
+        panel_id = data[1]
+        try:
+            panel_id = await verify_channel.fetch_message(panel_id)
+        except discord.NotFound:
+            panel_id = None
+        except discord.Forbidden:
+            await interaction.followup.send(f'I don\'t have permission to see the verification channels (<#{verify_channel_id}>) history.\nI need the "Read Message History" permission.', ephemeral = True)
+            return
+        if not panel_id:
+            try:
+                panel = await verify_channel.send(embed = embed, view = CaptchaView())
+            except discord.Forbidden:
+                await interaction.followup.send(f'I don\'t have permission to send messages in the verification channel (<#{verify_channel_id}>).', ephemeral = True)
+                return
+        else:
+            await interaction.followup.send('The verification panel is already sent.', ephemeral = True)
+            return
+    else:
+        try:
+            panel = await verify_channel.send(embed = embed, view = CaptchaView())
+        except discord.Forbidden:
+            await interaction.followup.send(f'I don\'t have permission to send messages in the verification channel (<#{verify_channel_id}>).', ephemeral = True)
+            return
+
+    c.execute('INSERT OR REPLACE INTO panels VALUES (?, ?)', (interaction.guild_id, panel.id))
+    conn.commit()
+    await interaction.followup.send(f'The verification panel has been sent to <#{verify_channel_id}>.', ephemeral = True)
+
+
+
+#Protect server
+@tree.command(name = 'setup', description = 'Setup the server for the bot.')
+@discord.app_commands.checks.cooldown(1, 60, key=lambda i: (i.guild_id))
+@discord.app_commands.checks.has_permissions(manage_guild = True)
+@discord.app_commands.describe(verify_channel = 'Channel for the verification message.', verify_role = 'Role assigned after successfull verification.', log_channel = 'Channel used to send logs.', timeout = 'After that timeframe the action gets executed.', action = 'Action that gets executed after timeout.')
+@discord.app_commands.choices(timeout = [
+    discord.app_commands.Choice(name = '5 Minutes', value = 300),
+    discord.app_commands.Choice(name = '10 Minutes', value = 600),
+    discord.app_commands.Choice(name = '15 Minutes', value = 900)
+    ],
+                            action = [
+    discord.app_commands.Choice(name = 'Kick', value = 'kick'),
+    discord.app_commands.Choice(name = 'Ban', value = 'ban'),
+    discord.app_commands.Choice(name = 'Nothing', value = '')
+    ])
+async def self(interaction: discord.Interaction, verify_channel: discord.TextChannel, verify_role: discord.Role, log_channel: discord.TextChannel, timeout: int, action: str):
+    #Check permissions
+    if not verify_channel.permissions_for(interaction.guild.me).send_messages:
+        await interaction.response.send_message(f'I need the permission to send messages in {verify_channel.mention}.', ephemeral=True)
+        return
+    #Check if bot can assign selected role
+    if not interaction.guild.me.top_role > verify_role:
+        await interaction.response.send_message(f'My highest role needs to be above {verify_role.mention}, so I can assign it.', ephemeral=True)
+        return
+    #Check if bot can send messages in log channel
+    if not log_channel.permissions_for(interaction.guild.me).send_messages:
+        await interaction.response.send_message(f'I need the permission to send messages in {log_channel.mention}.', ephemeral=True)
+        return
+    #Check if bot can ban or kick people (dependant on what action got selected)
+    if action == 'kick' or action == 'ban':
+        if not interaction.guild.me.guild_permissions.kick_members:
+            await interaction.response.send_message(f'I need the permission to {action} members.', ephemeral=True)
+            return
+    #Write data to database
+    c.execute('INSERT OR REPLACE INTO servers VALUES (?, ?, ?, ?, ?, ?)', (interaction.guild.id, verify_channel.id, verify_role.id, log_channel.id, timeout, action))
+    conn.commit()
+    await interaction.response.send_message(f'Setup completed.\nYou can now run `/send_panel`, to send the panel to <#{verify_channel.id}>.', ephemeral=True)
+
+
+
+#Show current settings
+@tree.command(name = 'settings', description = 'Show the current settings.')
+@discord.app_commands.checks.cooldown(1, 60, key=lambda i: (i.guild_id))
+@discord.app_commands.checks.has_permissions(manage_guild = True)
+async def self(interaction: discord.Interaction):
+    c.execute('SELECT * FROM servers WHERE guild_id = ?', (interaction.guild.id,))
+    data = c.fetchone()
+    if data:
+        verify_channel = data[1]
+        verify_role = data[2]
+        log_channel = data[3]
+        timeout = data[4]
+        action = data[5]
+        embed = discord.Embed(title = 'Current settings',
+                              description = f'**Verify Channel:** <#{verify_channel}>\n**Verify Role:** <@&{verify_role}>\n**Log Channel:** <#{log_channel}>\n**Timeout:** {timeout} Seconds\n**Action:** {action}',
+                              color = 0x2b63b0)
+        await interaction.response.send_message(embed = embed, ephemeral=True)
+    else:
+        await interaction.response.send_message('There are no settings for this server.\nUse `/setup` to set-up this server.', ephemeral=True)
+
+
 
 
 
